90eaf9248674616e37740dea089168f73efd1985
Gaurav-78 <gauravbhosale7801@gmail.com>
2024-11-13 03:51:52 +0530

    code to the many to many association with Automated testing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -52,12 +52,12 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
     def require_same_user
-    if current_user != @article.user
-      flash[:alert] = "You can only edit or delete your own article"
+    if current_user != @article.user && !current_user.admin?
+          flash[:alert] = "You can only edit or delete your own article"
       redirect_to @article
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_12_150452) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_12_204150) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
